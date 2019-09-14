46860748ccf2bf84d338d71e4e73490ecb387e58
mycoin2019 <mycoin2019@163.com>
2019-09-14 16:55:24 +0800

    change tx fee type

--- types/tx_type_txt.go
+++ types/tx_type_txt.go
@@ -162,7 +162,7 @@ func (tx *TokenTransaction) IllegalGasLimitOrGasPrice(hascode bool) bool {
 		if common.IsLKC(tx.data.TokenAddress) {
 			gasFee = CalNewAmountGas(tx.Value())
 		} else {
-			gasFee = MinGasLimit
+			gasFee = uint64(MinGasLimit)
 		}
 	}
 
