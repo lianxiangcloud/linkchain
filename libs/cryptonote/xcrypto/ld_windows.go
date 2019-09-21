27e317264b6a84425b5359d358ea394a16b92ace
gaspool1314 <gaspool1314@gmail.com>
2019-09-21 14:54:25 +0800

    add file for windows compiling

---
+++ libs/cryptonote/xcrypto/ld_windows.go
@@ -0,0 +1,4 @@
+package xcrypto
+
+// #cgo LDFLAGS:-lxcrypto -lboost_system-mt -lboost_filesystem-mt -lboost_thread-mt -lboost_date_time-mt -lboost_system-mt -lboost_regex-mt -lboost_chrono-mt -lsodium -lssl -lcrypto  -lstdc++ -lpthread -lws2_32 -lcrypt32 -lIphlpapi -lhidapi
+import "C"
