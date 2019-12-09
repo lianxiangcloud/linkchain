c33806c0ac7ae7fd5ac8f730f48f628e6e1bcb2b
jackru88888 <lut7914@gmail.com>
2019-12-09 20:42:00 +0800

    add sandbox start.sh

--- build.sh
+++ build.sh
@@ -18,14 +18,17 @@ function do_pack()
     mkdir -p $packdst/bin
     mkdir -p $packdst/data
     mkdir -p $packdst/sbin
-	mkdir -p $packdst/init
+	mkdir -p $packdst/init/sandbox
 
     chmod 777 bin/lkchain
     chmod 777 scripts/start.sh
     chmod 777 scripts/monitor.sh
+    chmod 777 scripts/sandbox_start.sh
     cp bin/lkchain $packdst/bin
     cp scripts/start.sh $packdst/sbin
+    cp scripts/sandbox_start.sh $packdst/sbin
     cp scripts/monitor.sh $packdst/sbin
+    cp test/genesis.json $packdst/init/sandbox
     cd pack ; tar zcf $tarfile $packfile ; echo "done $tarfile";
 }
 
---
+++ scripts/sandbox_start.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+proc=lkchain
+rootpath=$(dirname $(pwd))
+datapath=$rootpath/data
+logpath=$datapath/logs
+sandbox_genesis_file=$rootpath/init/sandbox/genesis.json
+emptyBlockInterval=300
+blockInterval=1000
+bootnode=https://sandbox-bootnode.lianxiangcloud.com
+
+function Init() {
+    if [ $# -ne 1 ]; then
+        echo "`Usage`"
+        exit 1;
+    fi
+    
+    if [ ! -L "$logpath" ]; then
+        mkdir -p "$logpath"
+    fi
+
+    $proc init --home $datapath  --genesis_file $sandbox_genesis_file --log.filename $logpath/lkchain.log
+}
+
+function Start() {
+     if [ $# -lt 1 ]; then
+        echo "`Usage`"
+        exit 1;
+    fi
+    rpcport=36000
+    wsport=38000
+    p2pport=37000
+    StartNode $@
+}
+
+
+function StartNode() {
+    echo "start $proc ..."
+    nohup $proc node --home $datapath  --bootnode.addrs $bootnode  --rpc.http_endpoint "127.0.0.1:$rpcport" --rpc.ws_endpoint "127.0.0.1:$wsport" --p2p.laddr "tcp://0.0.0.0:$p2pport" --consensus.create_empty_blocks_interval $emptyBlockInterval --consensus.timeout_commit $blockInterval --log.filename $logpath/lkchain.log --log_level debug > $logpath/error.log 2>&1 &
+    echo "pid: $!"
+}
+
+function Stop() {
+    if [ $# -ne 1 ]; then
+        echo "`Usage`"
+        exit 1;
+    fi
+
+    StopNode $@
+}
+
+function StopNode() {
+    pid=$(ps -ef | grep $proc |grep $datapath |grep -v grep | awk '{print $2}')
+    for i in $pid; do
+        echo "kill $i"
+        kill -9 $i 2> /dev/null
+    done
+}
+
+
+function Restart() {
+    Stop $@
+    Start $@
+}
+
+
+function Usage() {
+    echo ""
+    echo "USAGE:"
+    echo "command1: $0 init "
+    echo "          type: peer,validator,test"
+    echo ""
+    echo "command2: $0 start"
+    echo ""
+    echo ""
+    echo "command3: $0 stop "
+    echo ""
+    echo "command4: $0 restart"
+    echo ""
+}
+
+cd "$(dirname $0)"
+export PATH=$PWD/../bin:$PWD/:$PATH
+
+case $1 in
+    init) Init $@;;
+    start) Start $@;;
+    stop) Stop $@;;
+    restart) Restart $@;;
+    *) Usage;;
+esac
